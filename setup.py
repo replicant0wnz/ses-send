d37248cb600e90db515e2141a5be0650166787ce
Glenn E. Bailey III <337608+replicant0wnz@users.noreply.github.com>
2022-04-25 16:16:43 -0500

    Updated setup and added package info

--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
-from setuptools import setup, find_packages
-setup(
-    name = 'ses_send',
-    packages = find_packages("src")
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ses-send-replicant0wnz",
+    version="0.0.1",
+    author="Glenn E. Bailey III",
+    author_email="glenn@dronemusic.co",
+    description="Simple wrapper to send emails via AWS SES",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/replicant0wnz/ses-send",
+    project_urls={
+        "Bug Tracker": "https://github.com/replicant0wnz/ses-send/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
